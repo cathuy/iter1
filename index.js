c9b546a959ef434ba2a347809f543325bddffe1b
apirl <apirl@aprildeAir.hitronhub.home>
2019-10-29 21:31:03 -0700

    upload index.js

--- index.js
+++ index.js
@@ -2,22 +2,59 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var app = express();
-
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const bodyParser = require("body-parser");
 const { Pool } = require('pg');
 var pool;
 pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+    connectionString: 'postgres://postgres:129409Zydayy@localhost/PLAYER'
 });
 pool.connect();
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {res.render('pages/index')});
-app.get('/login', (req,res) => { res.render('pages/login')});;
-
+app.get('/', (req, res) => { res.render('pages/index') });
+app.get('/login', (req, res) => { res.render('pages/login') });
+//user login action
+app.post('/login_action', (req, res) => {
+    params = JSON.parse(JSON.stringify(req.body))
+    NAME = params['name']
+    PASSWORD = params['password']
+    var getUsersQuery = `SELECT * FROM public."Admin" WHERE "user_name" =  '${NAME}' and "password"='${PASSWORD}'`
+    pool.query(getUsersQuery, (error, result) => {
+        if (error) {
+            console.log(`this user is not existed`)
+            res.end(error);
+        }
+        console.log("this user is in our database");
+        res.render('/#t3')
+    });
+});
+//user sign up
+app.post('/signup_action', (req, res) => {
+    params = JSON.parse(JSON.stringify(req.body))
+    NAME = params['name']
+    PASSWORD = params['password']
+    confirm_ps = params['enter_password_again']
+    var Twins = 0
+    var Total_games = 0
+    if (PASSWORD == confirm_ps) {
+        insertQuery = `INSERT INTO public."Admin"("user_name", "password","Total_wins", "Total_games") 
+        VALUES( '${NAME}' , '${PASSWORD}', ${Twins} , ${Total_games})`
+        pool.query(insertQuery, function(err, result, fields) {
+            if (err) {
+                console.log("fail to sign up")
+                res.redirect('/');
+            } else {
+                console.log("success to sign up")
+                res.render('/')
+            }
+        });
+    } else { console.log("please enter password again") }
+});
 
 
 
