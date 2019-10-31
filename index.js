bed97e73c0f062baa5e98f62d721cc968ebc6d1f
Carinazhao22 <carinazhao2328@gmail.com>
2019-10-31 08:48:15 -0700

    a

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => { res.render('pages/chatRoom') });
+app.get('/', (req, res) => { res.render('public/chatRoom') });
  
  io.on('connection', (socket) => {
 
@@ -30,6 +29,7 @@ app.get('/', (req, res) => { res.render('pages/chatRoom') });
   socket.on('disconnect', () => console.log('Client disconnected'));
 
 });
+
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
  
