807a0bd7018b7921e6517da8e3ee2b867aba878b
Carinazhao22 <carinazhao2328@gmail.com>
2019-10-30 22:46:34 -0700

    A

--- index.js
+++ index.js
@@ -11,14 +11,21 @@ const server = app.listen(PORT, () => {
 });
 const io = require('socket.io')(server);
 
-io.on('connection', (socket) => {
+ io.sockets.on('connection', function(socket) {
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+    });
 
-  console.log('Client connected');
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+    })
 
-  socket.on('disconnect', () => console.log('Client disconnected'));
+    socket.on('chat_message', function(message) {
+        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    });
 
 });
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
   
 const INDEX = path.join(__dirname, 'public/index.html');
 app.use((req, res) => res.sendFile(INDEX) );
