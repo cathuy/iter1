2d81eb60d983f2903971078f131777e541c83ebe
SpicyChill <aprilchou9708@gmail.com>
2019-11-03 19:57:25 -0800

    update index

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
 const bodyParser = require("body-parser");
 const { Pool } = require('pg');
+ 
+
+const server = app.listen(PORT, () => {
+    console.log("Listening on port: " + PORT);
+});
+const io = require('socket.io')(server);
+
 var pool;
 pool = new Pool({
-    connectionString: 'postgres://postgres:129409Zydayy@localhost/PLAYER'
+    connectionString:  process.env.DATABASE_URL,
+    ssl: true
 });
 pool.connect();
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,22 +24,78 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => { res.render('pages/index') });
 
+ 
+ io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('chat',function(message){
+    console.log("chat message: "+ message);
+    io.emit('message',message);
+  });
+
+});
+
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+ 
+
+
+
+
+
+
 //user login action
+// app.post('/login_action', (req, res) => {
+//     params = JSON.parse(JSON.stringify(req.body))
+//     NAME = params['name']
+//     PASSWORD = params['password']
+//     var getUsersQuery = `SELECT * FROM Admin WHERE "user_name" =  '${NAME}' and "password"='${PASSWORD}'`
+//     if(NAME=='carinaA'&& PASSWORD=='123')//Admin
+      
+    
+//     pool.query(getUsersQuery, (error, result) => {
+//         if (error) {
+//             console.log(`this user is not existed`)
+//             res.end(error);
+//         }
+//         console.log("this user is in our database");
+//         res.render('pages/index')
+//     });
+// });
+ 
 app.post('/login_action', (req, res) => {
     params = JSON.parse(JSON.stringify(req.body))
     NAME = params['name']
     PASSWORD = params['password']
-    var getUsersQuery = `SELECT * FROM public."Admin" WHERE "user_name" =  '${NAME}' and "password"='${PASSWORD}'`
-    pool.query(getUsersQuery, (error, result) => {
+    var getAdminQuery = `SELECT * FROM Admin WHERE "username" =  '${NAME}' and "password"='${PASSWORD}'`
+    var getWhole = `SELECT * FROM Admin`;
+    pool.query(getAdminQuery, (error, result) => {
         if (error) {
             console.log(`this user is not existed`)
+            response.send('Incorrect Username and/or Password!');
             res.end(error);
         }
         console.log("this user is in our database");
-        res.render('pages/index')
+        //Admin
+        if (NAME != 'carinaA' && PASSWORD != '123') {
+            var results = { 'rows': result.rows };
+            console.log(results);
+            res.render('pages/gamer_data', results)
+        }
     });
+    if (NAME == 'carinaA' && PASSWORD == '123'){
+    pool.query(getWhole, (error, result) => {
+        if (error)
+            res.end(error);
+        var results = { 'rows': result.rows };
+        console.log(results);
+        res.render('pages/admin_data', results)
+    });}
+
 });
+
 //user sign up
+
 app.post('/signup_action', (req, res) => {
     params = JSON.parse(JSON.stringify(req.body))
     NAME = params['name']
@@ -42,13 +104,18 @@ app.post('/signup_action', (req, res) => {
     confirm_ps = params['enter_password_again']
     var Twins = 0
     var Total_games = 0
+    var TIME = NaN
+    var RESULT = 0
+    var SPY = 0
+    var WORD = NaN
     if (PASSWORD == confirm_ps) {
-        insertQuery = `INSERT INTO public."Admin"("user_name", "password","Total_wins", "Total_games") 
-        VALUES( '${NAME}' , '${PASSWORD}', ${Twins} , ${Total_games})`
+        insertQuery = `INSERT INTO Admin("username", "password","total_wins", "total_games","time","result","word","spy") 
+        VALUES( '${NAME}' , '${PASSWORD}', ${Twins} , ${Total_games}, '${TIME}', ${RESULT}, '${WORD}',${SPY})`
         pool.query(insertQuery, function(err, result, fields) {
             if (err) {
                 console.log("fail to sign up")
-                res.redirect('/');
+                // res.redirect('/');
+                res.send(err)
             } else {
                 console.log("success to sign up")
                 res.redirect('../#t3')
@@ -59,4 +126,4 @@ app.post('/signup_action', (req, res) => {
 
 
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+ 
