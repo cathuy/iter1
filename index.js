3bf6d02b88d9d888b67051610d6f4143ae85b6ae
Claire Jupiter <jupiteryao@icloud.com>
2019-11-04 20:57:53 -0800

    upd

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express')
+const app = express()
 const path = require('path')
+const http = require('http').Server(app)
+const socketIO = require('socket.io')
+const bodyParser = require("body-parser")
+
 const PORT = process.env.PORT || 5000
-var app = express();
-const bodyParser = require("body-parser");
+
+
+//database
 const { Pool } = require('pg');
- 
-
-const server = app.listen(PORT, () => {
-    console.log("Listening on port: " + PORT);
-});
-const io = require('socket.io')(server);
-
-var pool;
-pool = new Pool({
+var pool = new Pool({
     connectionString:  process.env.DATABASE_URL,
     ssl: true
 });
 pool.connect();
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,45 +24,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => { res.render('pages/index') });
 
- 
- io.on('connection', (socket) => {
+const server = app.listen(PORT, () => {
+    console.log("Listening on port: " + PORT);
+});
+
+const io = require('socket.io')(server);
+
+
+io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
   socket.on('chat',function(message){
     console.log("chat message: "+ message);
     io.emit('message',message);
   });
-
 });
 
-
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
- 
-
-
-
-
 
 
 //user login action
-// app.post('/login_action', (req, res) => {
-//     params = JSON.parse(JSON.stringify(req.body))
-//     NAME = params['name']
-//     PASSWORD = params['password']
-//     var getUsersQuery = `SELECT * FROM Admin WHERE "user_name" =  '${NAME}' and "password"='${PASSWORD}'`
-//     if(NAME=='carinaA'&& PASSWORD=='123')//Admin
-      
-    
-//     pool.query(getUsersQuery, (error, result) => {
-//         if (error) {
-//             console.log(`this user is not existed`)
-//             res.end(error);
-//         }
-//         console.log("this user is in our database");
-//         res.render('pages/index')
-//     });
-// });
- 
+
 app.post('/login_action', (req, res) => {
     params = JSON.parse(JSON.stringify(req.body))
     NAME = params['name']
@@ -109,7 +91,7 @@ app.post('/signup_action', (req, res) => {
     var SPY = 0
     var WORD = NaN
     if (PASSWORD == confirm_ps) {
-        insertQuery = `INSERT INTO Admin("username", "password","total_wins", "total_games","time","result","word","spy") 
+        insertQuery = `INSERT INTO Admin("username", "password","total_wins", "total_games","time","result","word","spy")
         VALUES( '${NAME}' , '${PASSWORD}', ${Twins} , ${Total_games}, '${TIME}', ${RESULT}, '${WORD}',${SPY})`
         pool.query(insertQuery, function(err, result, fields) {
             if (err) {
@@ -123,7 +105,3 @@ app.post('/signup_action', (req, res) => {
         });
     } else { console.log("please enter password again") }
 });
-
-
-
- 
